4324cf35ded8c92239f40a479abb8a69cc3c2f33
Raja Sekhar Vuyyuru <rajasekhar@instaread.co>
2024-02-03 12:02:41 +0530

    test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,20 @@ import CacheClient from "@/lib/Cache";
 // "use client";
 async function getData() {
   const key = "thirdparty";
-  const res = await CacheClient.get(key);
+  const res: any = await CacheClient.get(key);
   if (!res) {
     const data: any = await db.select().from(thirdparty);
     await CacheClient.set(key, data);
     return [...data];
   }
 
-  return res;
+  return JSON.parse(res);
 }
 export default async function Home() {
   const data = await getData();
-
   return (
     <div className="">
-      {data.map((e) => (
-        <p key={e.providerId}>{e?.provider}</p>
-      ))}
+      {data?.map((e: any) => <p key={e.providerId}>{e?.provider}</p>)}
     </div>
   );
 }
