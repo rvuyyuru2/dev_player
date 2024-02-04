2db9e70428defd292409bf802437d48056b8b39a
Raja Sekhar Vuyyuru <rajasekhar@instaread.co>
2024-02-04 16:40:50 +0530

    2wqewe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import db from "@/lib/database";
-import { thirdparty } from "../../drizzle/schema";
-import CacheClient from "@/lib/Cache";
+// import db from "@/lib/database";
+// import { thirdparty } from "../../drizzle/schema";
+// import CacheClient from "@/lib/Cache";
 
 // "use client";
 async function getData() {
-  const key = "thirdparty";
-  const res: any = await CacheClient.get(key);
+  // const key = "thirdparty";
+  // const res: any = await CacheClient.get(key);
 
-  if (!res) {
-    const data: any = await db.select().from(thirdparty);
-    await CacheClient.set(key, data);
-    return [...data];
-  }
+  // if (!res) {
+  //   const data: any = await db.select().from(thirdparty);
+  //   await CacheClient.set(key, data);
+  //   return [...data];
+  // }
   return [];
 }
 export default async function Home() {
