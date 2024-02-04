dfd933b10054e909697fea2e45c8cca84a4c05a9
Raja Sekhar Vuyyuru <rajasekhar@instaread.co>
2024-02-04 15:46:58 +0530

    u1

--- src/lib/Cache/index.ts
+++ src/lib/Cache/index.ts
@@ -4,5 +4,5 @@ import Redis from "ioredis";
 // By default, it will connect to localhost:6379.
 // We are going to cover how to specify connection options soon.
 const CacheClient = new Redis(process.env.CACHE_URL!);
-console.log(CacheClient.status);
+console.log(process.env.CACHE_URL!, CacheClient.status);
 export default CacheClient;
