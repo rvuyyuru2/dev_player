5209889479abfb9c31f63be1184c613ca764f9d3
Raja Sekhar Vuyyuru <rajasekhar@instaread.co>
2024-02-04 16:51:14 +0530

    wewewqe

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,7 @@
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
-
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { Vpc, SubnetType } from "aws-cdk-lib/aws-ec2";
 export default {
   config(_input) {
     return {
@@ -10,13 +11,29 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site", {
+      // Create a VPC
+      const vpc = new Vpc(stack, "vpc-a77ee8c3");
+
+      // Alternatively use an existing VPC
+
+      const vpcSubnets = {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      };
+
+      const site = new NextjsSite(stack, "Site", {
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
           CACHE_URL: process.env.CACHE_URL!,
         },
         edge: true,
         runtime: "nodejs20.x",
+        cdk: {
+          server: {
+            vpc,
+            vpcSubnets,
+            logRetention: RetentionDays.ONE_MONTH,
+          },
+        },
       });
 
       stack.addOutputs({
